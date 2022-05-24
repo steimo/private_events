dd2a47a5f2ba08b3f14c76c6e14a6e77a7c52f2a
steimz <vladyslav.kolomiets@protonmail.com>
2022-05-24 22:33:31 +0300

    Add links to header

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-  @event = current_user.events.build
+  @event = current_user.created_events.build
   end
 
   # GET /events/1/edit
@@ -20,7 +20,7 @@ class EventsController < ApplicationController
 
   # POST /events or /events.json
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     respond_to do |format|
       if @event.save
