aa117364fdabf5245f3237d2efc239eab0113a26
steimz <vladyslav.kolomiets@protonmail.com>
2022-05-26 22:18:41 +0300

    Add style to event form and minor changes to main view

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+         backgroundImage: theme => ({
+         'event-back-img': "url('https://images.unsplash.com/photo-1535430046985-b806390339ba?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2063')",
+        })
       },
     },
   },
