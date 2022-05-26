259134f74ff7d0d08e87accb2845e308b7f7c406
steimz <vladyslav.kolomiets@protonmail.com>
2022-05-26 13:55:26 +0300

    Add name column and validation to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
   has_many :invitations, foreign_key: 'attendee_id'
   has_many :attended_events, through: :invitations, source: :event
+  before_save :downcase_email
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Converts email to all lowercase.
+  def downcase_email
+    self.email = email.downcase
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "events#index"
+  root 'events#index'
   get 'users/:user_id/created_events' => 'users#created_events', as: :user_events
 end
